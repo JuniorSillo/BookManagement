aafeba2c436e2387709dc8bd305ff92e11b2aff0
JuniorSillo <123242158+JuniorSillo@users.noreply.github.com>
2024-08-19 08:31:28 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
